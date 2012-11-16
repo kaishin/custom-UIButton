b6b717703e63e61451c7224da74149c2ff50881b
Reda Lemeden <reda@thoughtbot.com>
2012-11-16 02:30:23 +0000

    Implement CBHybrid

---
+++ Custom UIButtons/CBHybrid.m
@@ -0,0 +1,104 @@
+#import "CBHybrid.h"
+
+@implementation CBHybrid
+
+static UIImage *gBackgroundImage;
+static UIImage *gBackgroundImageHighlighted;
+static int borderRadius = 5;
+static int height = 37;
+
+#pragma mark - UIButton Overrides
+
++ (CBHybrid *)buttonWithType:(UIButtonType)type
+{
+    return [super buttonWithType:UIButtonTypeCustom];
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setupBackgrounds];
+    }
+    
+    return self;
+}
+
+#pragma mark - Touch event overrides
+
+- (void)setupBackgrounds {
+    
+    if (!gBackgroundImage && !gBackgroundImageHighlighted) {
+        gBackgroundImage = [[self drawBackgroundImageHighlighted:NO] resizableImageWithCapInsets:UIEdgeInsetsMake(borderRadius, borderRadius, borderRadius, borderRadius) resizingMode:UIImageResizingModeStretch];
+        gBackgroundImageHighlighted = [[self drawBackgroundImageHighlighted:YES] resizableImageWithCapInsets:UIEdgeInsetsMake(borderRadius, borderRadius, borderRadius, borderRadius) resizingMode:UIImageResizingModeStretch];
+    }
+    
+    [self setBackgroundImage:gBackgroundImage forState:UIControlStateNormal];
+    [self setBackgroundImage:gBackgroundImageHighlighted forState:UIControlStateHighlighted];
+}
+
+- (UIImage *)drawBackgroundImageHighlighted:(BOOL)highlighted {
+    
+    float width = 1 + (borderRadius * 2);
+    
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(width, height), NO, 0.0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    // Color Declarations
+    UIColor *borderColor = [UIColor colorWithRed:0.77f green:0.43f blue:0.00f alpha:1.00f];
+    UIColor *topColor = [UIColor colorWithRed:0.94f green:0.82f blue:0.52f alpha:1.00f];
+    UIColor *bottomColor = [UIColor colorWithRed:0.91f green:0.55f blue:0.00f alpha:1.00f];
+    UIColor *innerGlow = [UIColor colorWithWhite:1.0 alpha:0.5];
+    
+    // Gradient Declarations
+    NSArray *gradientColors = (@[
+                               (id)topColor.CGColor,
+                               (id)bottomColor.CGColor
+                               ]);
+    
+    CGGradientRef gradient = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef)(gradientColors), NULL);
+    
+    NSArray *highlightedGradientColors = (@[
+                                          (id)bottomColor.CGColor,
+                                          (id)topColor.CGColor
+                                          ]);
+    
+    CGGradientRef highlightedGradient = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef)(highlightedGradientColors), NULL);
+    
+    
+    // Draw rounded rectangle bezier path
+    UIBezierPath *roundedRectanglePath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(0, 0, width, height) cornerRadius: borderRadius];
+    // Use the bezier as a clipping path
+    [roundedRectanglePath addClip];
+    
+    // Use one of the two gradients depending on the state of the button
+    CGGradientRef background = highlighted? highlightedGradient : gradient;
+    
+    // Draw gradient within the path
+    CGContextDrawLinearGradient(context, background, CGPointMake(140, 0), CGPointMake(140, height-1), 0);
+    
+    // Draw border
+    [borderColor setStroke];
+    roundedRectanglePath.lineWidth = 2;
+    [roundedRectanglePath stroke];
+    
+    // Draw Inner Glow
+    UIBezierPath *innerGlowRect = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(1.5, 1.5, width - 3, height - 3) cornerRadius: borderRadius * 4/5];
+    [innerGlow setStroke];
+    innerGlowRect.lineWidth = 1;
+    [innerGlowRect stroke];
+    
+    // Output as Image
+    UIImage* backgroundImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    // Cleanup
+    CGGradientRelease(gradient);
+    CGGradientRelease(highlightedGradient);
+    CGColorSpaceRelease(colorSpace);
+    
+    return backgroundImage;
+}
+
+@end
